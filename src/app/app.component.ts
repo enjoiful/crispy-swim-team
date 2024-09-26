e25a2500dafae900037017b87e37dbf20797e3ce
enjoiful <enjoiful@users.noreply.github.com>
2024-09-26 14:45:39 -0700

    chart js added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,12 +5,14 @@ import { FirebaseService } from './services/firebase.service';
 import { CommonModule } from '@angular/common';  // Import CommonModule
 import { FormsModule } from '@angular/forms';  // <-- Import FormsModule
 import { environment } from '../environments/environment';
+import { BaseChartDirective } from 'ng2-charts';
+
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, FormsModule],
+  imports: [RouterOutlet, CommonModule, FormsModule, BaseChartDirective],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   providers: [FirebaseService], // Ensure your service is properly injected
@@ -20,6 +22,33 @@ export class AppComponent implements OnInit {
   title = 'Crispy Swim Team';  // Added title property
   sessions: any[] = [];
   selectedDate: string = '';  // Bound to the date input field
+
+  barChartData = {
+    "labels": ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    "datasets": [
+      {
+        "label": "Votes",
+        "data": [12, 19, 3, 5, 2, 3],
+        "backgroundColor": [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)"
+        ],
+        "borderColor": [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)"
+        ],
+        "borderWidth": 1
+      }
+    ]
+  }
 
   constructor(private firebaseService: FirebaseService) {
     initializeApp(environment.firebaseConfig);
--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,11 @@ import { provideDatabase, getDatabase } from '@angular/fire/database';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
+import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
 
   bootstrapApplication(AppComponent, {
     providers: [
       provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
-      provideDatabase(() => getDatabase()),
+      provideDatabase(() => getDatabase()), provideCharts(withDefaultRegisterables()),
     ],
   }).catch((err) => console.error(err));
