24e86cd41cf7069e6f04567453d9cbc76d550131
enjoiful <enjoiful@users.noreply.github.com>
2024-10-03 09:58:11 -0700

    session overview instead of details

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes, RouterModule} from '@angular/router';
 import { NgModule } from '@angular/core';
 import { DashboardCardComponent} from './dashboard-card/dashboard-card.component'
+import { AppComponent} from './app.component'
 
 import { SiteDetailsComponent} from './site-details/site-details.component'
 import { AllSitesComponent} from './all-sites/all-sites.component'
 
 export const routes: Routes = [
+    { path: 'old', component: AppComponent },
     { path: 'component1', component: DashboardCardComponent },
     { path: 'all-sites', component: AllSitesComponent },
     { path: 'site-details', component: SiteDetailsComponent },
